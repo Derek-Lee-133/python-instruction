b03ffa4a43b72a4cd9dd8d0e5db0b7d173cfcbb0
Derek Lee <dlee133@gmail.com>
2021-03-08 11:08:30 -0500

    init

---
+++ escapechar.py
@@ -0,0 +1,18 @@
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
+
+tabbedString = "1\t2\t3\t4\t5"
+print(tabbedString)
+
+print('The pet shop owner said "No, no, \'e\'s uh,...he\'s resting".')
+
+print("the pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting".""")
+
+anotherSplitString = """this string has been \
+split over \
+several \
+lines"""
+
+print(anotherSplitString)
---
+++ strings.py
@@ -0,0 +1,12 @@
+print('Today is a good day to learn Python')
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + "world")
+greeting = "Hello"
+name = input("Please enter your name ")
+print (greeting + name)
+
+print(greeting + ' ' + name)
+
+
