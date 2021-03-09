9a3664beb3e9de65a02798c0094fc643e97a3ac8
Derek Lee <dlee133@gmail.com>
2021-03-09 13:05:39 -0500

    slicing strings

--- strings2.py
+++ strings2.py
@@ -1,32 +1,11 @@
 parrot = "Norwegian Blue"
-print(parrot)
 
-print(parrot[3])
-print(parrot[4])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
+print(parrot[0:6:2])    # Nre
+print(parrot[0:6:3])    # Nw
 
-print()
+number = "9,223;372:036 854,775;807"
+seperators = number[1::4]
+print(seperators)
 
-print(parrot[-11])
-print(parrot[-1])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
-
-print()
-
-print(parrot[3 - 14])
-print(parrot[4 - 14])
-print(parrot[9 - 14])
-print(parrot[3 - 14])
-print(parrot[6 - 14])
-print(parrot[8 - 14])
-
-
-
-
-
+values = "".join(char if char not in seperators else " " for char in number).split()
+print([int(val) for val in values])
